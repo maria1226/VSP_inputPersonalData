808dc016a6b9e9d1d0d43cc04830e8f4f4a5603e
maria1226 <56468069+maria1226@users.noreply.github.com>
2020-10-14 19:24:52 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,9 +8,7 @@ namespace VSP_inputPersonalData
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+       
         [STAThread]
         static void Main()
         {
